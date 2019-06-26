51cfa1567a208f690fc8ff3e33bc1e56da314fec
adriaroca <a.roca.rada@gmail.com>
2019-06-26 17:52:51 +0200

    check if helper imaginatorGenUrls exists

--- src/helpers/ImaginatorGenUrls.php
+++ src/helpers/ImaginatorGenUrls.php
@@ -2,51 +2,62 @@
 
 use Omatech\Imaginator\Repositories\Imaginator;
 
-function imaginatorGenUrls($id, $alt = '', $formats = [], $options = [], $sets = [])
-{
-    $imaginator = new Imaginator();
+if (! function_exists('imaginatorGenUrls')) {
 
-    $html = '';
-    $urls = '';
+    /**
+     * @param $id
+     * @param string $alt
+     * @param array $formats
+     * @param array $options
+     * @param array $sets
+     * @return string
+     */
+    function imaginatorGenUrls($id, $alt = '', $formats = [], $options = [], $sets = [])
+    {
+        $imaginator = new Imaginator();
 
-    foreach ($formats as $format) {
-        foreach ($sets as $index => $set) {
-            if (!empty($set['srcset'])) {
-                if (gettype($set['srcset']) == 'array') {
-                    $srcset = $set['srcset'];
-                } elseif (gettype($set['srcset']) === 'integer') {
-                    $srcset = [$set['srcset'] => $set['srcset']];
+        $html = '';
+        $urls = '';
+
+        foreach ($formats as $format) {
+            foreach ($sets as $index => $set) {
+                if (!empty($set['srcset'])) {
+                    if (gettype($set['srcset']) == 'array') {
+                        $srcset = $set['srcset'];
+                    } elseif (gettype($set['srcset']) === 'integer') {
+                        $srcset = [$set['srcset'] => $set['srcset']];
+                    } else {
+                        $srcset = [];
+                    }
                 } else {
                     $srcset = [];
                 }
-            } else {
-                $srcset = [];
+
+                $html .= "<source";
+
+                $urls = $imaginator->generateUrls([
+                    'hash' => $id,
+                    'srcset' => $srcset ?? [],
+                    'media' => $set['media'] ?? '',
+                    'sizes' => $set['sizes'] ?? '',
+                    'options' => $options ?? [],
+                    'format' => $format
+                ]);
+
+                if (!empty($set['media'])) {
+                    $html .= " media='{$set['media']}'";
+                }
+
+                if (!empty($set['sizes'])) {
+                    $html .= " size='{$set['sizes']}'";
+                }
+
+                $html .= " srcset='{$urls['srcset']}' type='image/{$urls['format']}'>\n";
             }
-
-            $html .= "<source";
-
-            $urls = $imaginator->generateUrls([
-                'hash'    => $id,
-                'srcset'  => $srcset ?? [],
-                'media'   => $set['media'] ?? '',
-                'sizes'   => $set['sizes'] ?? '',
-                'options' => $options ?? [],
-                'format'  => $format
-            ]);
-
-            if (!empty($set['media'])) {
-                $html .= " media='{$set['media']}'";
-            }
-
-            if (!empty($set['sizes'])) {
-                $html .= " size='{$set['sizes']}'";
-            }
-
-            $html .= " srcset='{$urls['srcset']}' type='image/{$urls['format']}'>\n";
         }
-    }
 
-    $html .= "<img src='{$imaginator->generateUrls(['hash' => $id, 'options'=> $options])['base']}' alt='{$alt}'>";
-    
-    return $html;
+        $html .= "<img src='{$imaginator->generateUrls(['hash' => $id, 'options'=> $options])['base']}' alt='{$alt}'>";
+
+        return $html;
+    }
 }
