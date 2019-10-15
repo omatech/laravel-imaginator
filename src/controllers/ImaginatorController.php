1f0ddf395224a72ff8fd3f159ecbec491182054a
Christian <cbohollo@omatech.com>
2019-10-15 20:31:50 +0200

    Trying remove nocache options

--- src/controllers/ImaginatorController.php
+++ src/controllers/ImaginatorController.php
@@ -9,6 +9,11 @@ class ImaginatorController extends Controller
 {
     public function get(Imaginator $imaginator, $path)
     {
+        session_write_close();
+        session_cache_limiter('public');
+        session_start();
+        header_remove( 'Pragma' );
+        
         return $imaginator->getProcessedImage($path, request()->all());
     }
 }
